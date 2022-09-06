083b66e8b0ae6a1fe7957a2309c8234a47dd3e99
ShehrozEdu <battshehrozedu@gmail.com>
2022-09-06 21:40:19 +0530

    Paytm testing

--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const productController = require("../controllers/productController");
 const UserController = require("../controllers/UserController");
+const PaymentController = require("../controllers/payment-controller");
+
 //Products
 router.post("/add-products", productController.addProducts);
 router.get("/get-products", productController.getProducts);
@@ -13,5 +15,9 @@ router.get("/get-product-by-id/:id", productController.getProductByID);
 //Users
 router.post("/signup", UserController.signUp);
 router.post("/login", UserController.login);
+
+//patym
+// router.post("/payment", PaymentController.addPaymentGateway);
+// router.post("/callback", PaymentController.paymentResponse);
 
 module.exports = router;
